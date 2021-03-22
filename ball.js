88bb3da4fd4fae26875b2e3923c7725d8a4d8247
suhascode <80819355+suhascode@users.noreply.github.com>
2021-03-22 20:04:23 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,35 @@
+class Ball {
+    constructor(x,y,width,height){
+      var  options={
+       
+        
+        density:1,
+        frictionAir:0.008
+      }
+      
+      this.body = Bodies.rectangle(x,y,width,height,options)
+      this.width = width
+      this.height = height
+      World.add(world,this.body);
+
+    }
+display(){
+var pos = this.body.position
+var angle = this.body.angle
+push();
+translate(pos.x,pos.y);
+rotate(angle);
+rectMode(CENTER);
+stroke("white");
+fill("black");
+ellipse(0,0,this.width,this.height);
+pop();
+
+}
+
+
+
+
+
+
+}
